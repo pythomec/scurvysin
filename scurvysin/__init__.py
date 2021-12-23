423602fdc3654fdc248dc7948b7d23269fb4e0b5
Jan Pipek <jan.pipek@gmail.com>
2021-12-23 11:22:35 +0100

    Enable mamba

--- scurvysin/__init__.py
+++ scurvysin/__init__.py
@@ -14,7 +14,20 @@ from pkg_resources import Requirement, Environment
 from typing import Dict, List
 
 
-__version__ = "0.1.3"
+__version__ = "2021.0"
+
+
+MAMBA_AVAILABLE = subprocess.getstatusoutput("mamba -V")[0] == 0
+CONDA_AVAILABLE = subprocess.getstatusoutput("conda -V")[0] == 0
+
+if not CONDA_AVAILABLE and not MAMBA_AVAILABLE:
+    print("Neither conda nor mamba is available.")
+    exit(1)
+
+if MAMBA_AVAILABLE and not os.environ.get("DISABLE_MAMBA"):
+    CONDA_COMMAND = "mamba"
+else:
+    CONDA_COMMAND = "conda"
 
 
 class CondaFlags:
@@ -58,14 +71,14 @@ def already_satisfied(req: str) -> bool:
 
 
 def available_in_conda(req: str) -> bool:
-    r = subprocess.run(["conda", "install", "-d", req],
+    r = subprocess.run([CONDA_COMMAND, "install", "-d", req],
                        stderr=subprocess.PIPE,
                        stdout=subprocess.PIPE)
     return not(r.returncode)
 
 
 def install_using_conda(req: str, flags: CondaFlags) -> None:
-    subprocess.call(["conda", "install", "-S", "-y"] + flags.expfl() + [req])
+    subprocess.call([CONDA_COMMAND, "install", "-S", "-y"] + flags.expfl() + [req])
 
 
 def install_using_pip(req: str, flags: PipFlags) -> None:
@@ -114,13 +127,13 @@ def try_install(req: str, opts: dict, coflags: CondaFlags, pipflags: PipFlags) -
     if already_satisfied(req):
         print(f"Condition {req} already satistfied.")
         return
-    print(f"Checking {req} in conda...")
+    print(f"Checking {req} using {CONDA_COMMAND}...")
     if available_in_conda(req):
-        print(f"Package {req} found in conda.")
+        print(f"Package {req} found with {CONDA_COMMAND}.")
         if opts["show_only"]:
-            print(f"Would install {req} using conda.")
+            print(f"Would install {req} using {CONDA_COMMAND}.")
         else:
-            print(f"Installing {req} using conda.")
+            print(f"Installing {req} using {CONDA_COMMAND}.")
             install_using_conda(req, coflags)
     else:
         print(f"Checking dependencies for {req} using pip...")
